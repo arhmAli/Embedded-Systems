5c548ea31d5b642f621fc9310f4458effdb2c9ab
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-06 16:49:13 +0500

    created the loop in asembly

--- loop.asm
+++ loop.asm
@@ -4,7 +4,7 @@ BCF TRISB,7
 SETF TRISD
 MOVLW 0X255
 MOVWF TRISD
-HERE DECFSZ TRISD
+HERE DECFSZ TRISD //decrement the specific file register skip the next line if the register has zero value
 BRA HERE
 MOVFF TRISD TRISB,7
 END
