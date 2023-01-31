d55dfadcf6ce80461c5d42741b3bc2b191c82a6d
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-31 20:04:35 +0500

    controlled the speed of a DC motor

--- arduino/controlSpeed.C
+++ arduino/controlSpeed.C
@@ -1,4 +1,5 @@
-// The following program controls the direction of the DC motor first the motor goes clockwisedirection control
+// The following program controls the direction of the DC motor first the motor goes clockwise & after some delay it stops 
+// Then it rotates in the counter clock wise direction
 //This program is made in MATLAB IDE but given C extension so that it can run in arduino IDE easily
 
 a=arduino()
